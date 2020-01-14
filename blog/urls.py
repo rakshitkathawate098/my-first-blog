704b08f8ee247190e531f3b0689f6b26726bd377
Rakshit <rkathawate@vmware.com>
2020-01-14 11:51:20 +0530

    Added Views for the individual posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# post_list will now become the default homepage
 urlpatterns=[
-        path('',views.post_list,name='post_list')
+        path('',views.post_list,name='post_list'),
+        path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
